56a2ccc7f218776b68c57d7435c0956386c27100
johnturnham <support@wallet.inc>
2026-07-05 22:26:32 +0000

    v2.1.604

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Wallet Inc. API reference.
 
-**Spec version 2.1.604**, built 2026-07-05T11:48:06.451Z
+**Spec version 2.1.604**, built 2026-07-05T22:22:40.700Z
 
 
 ## Installation & Usage
--- lib/Api/EmailSubscribersApi.php
+++ lib/Api/EmailSubscribersApi.php
@@ -12,7 +12,7 @@
 /**
  * wallet-api
  *
- * Wallet Inc. API reference.  **Spec version 2.1.604**, built 2026-07-05T11:48:06.451Z
+ * Wallet Inc. API reference.  **Spec version 2.1.604**, built 2026-07-05T22:22:40.700Z
  *
  * The version of the OpenAPI document: 2.1.604
  * Contact: development@wallet.inc
