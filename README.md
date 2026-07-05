2568de2b40209a235b2e201814edb1d36bf8695e
johnturnham <support@wallet.inc>
2026-07-05 10:25:04 +0000

    v2.1.604

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Wallet Inc. API reference.
 
-**Spec version 2.1.604**, built 2026-07-05T10:16:15.495Z
+**Spec version 2.1.604**, built 2026-07-05T10:21:15.333Z
 
 
 ## Installation & Usage
--- lib/Model/ExportDataFile.php
+++ lib/Model/ExportDataFile.php
@@ -13,7 +13,7 @@
 /**
  * wallet-api
  *
- * Wallet Inc. API reference.  **Spec version 2.1.604**, built 2026-07-05T10:16:15.495Z
+ * Wallet Inc. API reference.  **Spec version 2.1.604**, built 2026-07-05T10:21:15.333Z
  *
  * The version of the OpenAPI document: 2.1.604
  * Contact: development@wallet.inc
