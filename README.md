dd0460affae9418a60f9748f85870a66bb113d7e
johnturnham <support@wallet.inc>
2026-07-06 11:11:28 +0000

    v2.1.604

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Wallet Inc. API reference.
 
-**Spec version 2.1.604**, built 2026-07-05T23:47:46.990Z
+**Spec version 2.1.604**, built 2026-07-06T11:07:48.885Z
 
 
 ## Installation & Usage
--- lib/Model/ClickFunnelContactProfile.php
+++ lib/Model/ClickFunnelContactProfile.php
@@ -13,7 +13,7 @@
 /**
  * wallet-api
  *
- * Wallet Inc. API reference.  **Spec version 2.1.604**, built 2026-07-05T23:47:46.990Z
+ * Wallet Inc. API reference.  **Spec version 2.1.604**, built 2026-07-06T11:07:48.885Z
  *
  * The version of the OpenAPI document: 2.1.604
  * Contact: development@wallet.inc
