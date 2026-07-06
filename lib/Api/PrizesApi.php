eb33a3914a817afdd3d266d72922545ca3e11c9d
johnturnham <support@wallet.inc>
2026-07-06 15:52:28 +0000

    v2.1.604

--- lib/Api/PrizesApi.php
+++ lib/Api/PrizesApi.php
@@ -12,7 +12,7 @@
 /**
  * wallet-api
  *
- * Wallet Inc. API reference.  **Spec version 2.1.604**, built 2026-07-06T15:37:07.333Z
+ * Wallet Inc. API reference.  **Spec version 2.1.604**, built 2026-07-06T15:48:48.361Z
  *
  * The version of the OpenAPI document: 2.1.604
  * Contact: development@wallet.inc
