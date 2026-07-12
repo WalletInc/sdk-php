3a8c2ca39568f4142183a188ea0f12e311923426
johnturnham <support@wallet.inc>
2026-07-12 14:24:30 +0000

    v2.4.1

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Wallet Inc. API reference.
 
-**Spec version 2.4.1**, built 2026-07-12T14:06:52.477Z
+**Spec version 2.4.1**, built 2026-07-12T14:20:49.899Z
 
 
 ## Installation & Usage
--- lib/Api/HelpDeskApi.php
+++ lib/Api/HelpDeskApi.php
@@ -12,7 +12,7 @@
 /**
  * wallet-api
  *
- * Wallet Inc. API reference.  **Spec version 2.4.1**, built 2026-07-12T14:06:52.477Z
+ * Wallet Inc. API reference.  **Spec version 2.4.1**, built 2026-07-12T14:20:49.899Z
  *
  * The version of the OpenAPI document: 2.4.1
  * Contact: development@wallet.inc
