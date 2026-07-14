f400f4f89f3a5a454332aaff5966ba607b5399a4
johnturnham <support@wallet.inc>
2026-07-14 03:47:01 +0000

    v2.4.1

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Wallet Inc. API reference.
 
-**Spec version 2.4.1**, built 2026-07-14T03:37:32.564Z
+**Spec version 2.4.1**, built 2026-07-14T03:43:13.036Z
 
 
 ## Installation & Usage
--- lib/Api/SMSSubscribersApi.php
+++ lib/Api/SMSSubscribersApi.php
@@ -12,7 +12,7 @@
 /**
  * wallet-api
  *
- * Wallet Inc. API reference.  **Spec version 2.4.1**, built 2026-07-14T03:37:32.564Z
+ * Wallet Inc. API reference.  **Spec version 2.4.1**, built 2026-07-14T03:43:13.036Z
  *
  * The version of the OpenAPI document: 2.4.1
  * Contact: development@wallet.inc
