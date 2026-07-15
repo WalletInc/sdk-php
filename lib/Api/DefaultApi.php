f5ca81f17948cd276518aa93f12abe4c0e42a3e8
johnturnham <support@wallet.inc>
2026-07-15 19:52:38 +0000

    v2.4.1

--- lib/Api/DefaultApi.php
+++ lib/Api/DefaultApi.php
@@ -12,7 +12,7 @@
 /**
  * wallet-api
  *
- * Wallet Inc. API reference.  **Spec version 2.4.1**, built 2026-07-15T18:16:02.033Z
+ * Wallet Inc. API reference.  **Spec version 2.4.1**, built 2026-07-15T19:49:02.066Z
  *
  * The version of the OpenAPI document: 2.4.1
  * Contact: development@wallet.inc
