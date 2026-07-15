e331f46fc79ccb205f00d90fc561b030c61a6baa
johnturnham <support@wallet.inc>
2026-07-15 13:34:46 +0000

    v2.4.1

--- lib/Model/SubscriptionFeature.php
+++ lib/Model/SubscriptionFeature.php
@@ -13,7 +13,7 @@
 /**
  * wallet-api
  *
- * Wallet Inc. API reference.  **Spec version 2.4.1**, built 2026-07-15T02:41:13.074Z
+ * Wallet Inc. API reference.  **Spec version 2.4.1**, built 2026-07-15T13:30:53.626Z
  *
  * The version of the OpenAPI document: 2.4.1
  * Contact: development@wallet.inc
