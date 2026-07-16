0c5b45b429a9237a299f13bcf98c8bced681f494
johnturnham <support@wallet.inc>
2026-07-16 15:17:16 +0000

    v2.4.1

--- lib/Model/InternalServerError500.php
+++ lib/Model/InternalServerError500.php
@@ -13,7 +13,7 @@
 /**
  * wallet-api
  *
- * Wallet Inc. API reference.  **Spec version 2.4.1**, built 2026-07-15T19:49:02.066Z
+ * Wallet Inc. API reference.  **Spec version 2.4.1**, built 2026-07-16T15:13:35.242Z
  *
  * The version of the OpenAPI document: 2.4.1
  * Contact: development@wallet.inc
