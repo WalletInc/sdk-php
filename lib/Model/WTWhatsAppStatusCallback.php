cabc346099564cb866a79cdd5673bcc2b518caba
johnturnham <support@wallet.inc>
2026-07-16 10:22:11 +0000

    v2.4.1

--- lib/Model/WTWhatsAppStatusCallback.php
+++ lib/Model/WTWhatsAppStatusCallback.php
@@ -13,7 +13,7 @@
 /**
  * wallet-api
  *
- * Wallet Inc. API reference.  **Spec version 2.4.1**, built 2026-07-16T10:09:10.924Z
+ * Wallet Inc. API reference.  **Spec version 2.4.1**, built 2026-07-16T10:18:24.081Z
  *
  * The version of the OpenAPI document: 2.4.1
  * Contact: development@wallet.inc
