c868ea6b43494d867a220f4456462de1c2490076
johnturnham <support@wallet.inc>
2026-07-16 10:36:50 +0000

    v2.4.1

--- lib/Api/StaticVouchersApi.php
+++ lib/Api/StaticVouchersApi.php
@@ -12,7 +12,7 @@
 /**
  * wallet-api
  *
- * Wallet Inc. API reference.  **Spec version 2.4.1**, built 2026-07-16T10:18:24.081Z
+ * Wallet Inc. API reference.  **Spec version 2.4.1**, built 2026-07-16T10:33:29.487Z
  *
  * The version of the OpenAPI document: 2.4.1
  * Contact: development@wallet.inc
