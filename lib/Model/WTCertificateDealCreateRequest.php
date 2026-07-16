1f366863896c10e88ba1fca968863b9598fbc4d0
johnturnham <support@wallet.inc>
2026-07-16 19:53:32 +0000

    v2.4.1

--- lib/Model/WTCertificateDealCreateRequest.php
+++ lib/Model/WTCertificateDealCreateRequest.php
@@ -13,7 +13,7 @@
 /**
  * wallet-api
  *
- * Wallet Inc. API reference.  **Spec version 2.4.1**, built 2026-07-16T19:39:54.387Z
+ * Wallet Inc. API reference.  **Spec version 2.4.1**, built 2026-07-16T19:50:01.701Z
  *
  * The version of the OpenAPI document: 2.4.1
  * Contact: development@wallet.inc
