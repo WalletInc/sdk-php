de6cc90299cf23472ec144a9c1b0142eb3a828d6
johnturnham <support@wallet.inc>
2026-07-16 12:05:51 +0000

    v2.4.1

--- lib/Api/PromotionCodesApi.php
+++ lib/Api/PromotionCodesApi.php
@@ -12,7 +12,7 @@
 /**
  * wallet-api
  *
- * Wallet Inc. API reference.  **Spec version 2.4.1**, built 2026-07-16T11:49:20.813Z
+ * Wallet Inc. API reference.  **Spec version 2.4.1**, built 2026-07-16T12:02:06.305Z
  *
  * The version of the OpenAPI document: 2.4.1
  * Contact: development@wallet.inc
