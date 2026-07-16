64f44dbcc9a9ebdafd81851557c09c5d5ad6ffc5
johnturnham <support@wallet.inc>
2026-07-16 13:51:07 +0000

    v2.4.1

--- lib/Model/WTFetchWalletPaymentObjectsWithToken.php
+++ lib/Model/WTFetchWalletPaymentObjectsWithToken.php
@@ -13,7 +13,7 @@
 /**
  * wallet-api
  *
- * Wallet Inc. API reference.  **Spec version 2.4.1**, built 2026-07-16T13:38:13.052Z
+ * Wallet Inc. API reference.  **Spec version 2.4.1**, built 2026-07-16T13:47:28.968Z
  *
  * The version of the OpenAPI document: 2.4.1
  * Contact: development@wallet.inc
