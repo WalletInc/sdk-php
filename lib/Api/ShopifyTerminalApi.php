9daa4d1436afa6dc0a0cd4af31af951d7b042c1c
johnturnham <support@wallet.inc>
2026-07-16 11:52:59 +0000

    v2.4.1

--- lib/Api/ShopifyTerminalApi.php
+++ lib/Api/ShopifyTerminalApi.php
@@ -12,7 +12,7 @@
 /**
  * wallet-api
  *
- * Wallet Inc. API reference.  **Spec version 2.4.1**, built 2026-07-16T11:09:49.017Z
+ * Wallet Inc. API reference.  **Spec version 2.4.1**, built 2026-07-16T11:49:20.813Z
  *
  * The version of the OpenAPI document: 2.4.1
  * Contact: development@wallet.inc
