e622aa4bc4320ce7a30b62b8a755aaa79a85c755
johnturnham <support@wallet.inc>
2026-07-22 17:44:53 +0000

    v2.4.1

--- lib/Model/Performance.php
+++ lib/Model/Performance.php
@@ -13,7 +13,7 @@
 /**
  * wallet-api
  *
- * Wallet Inc. API reference.  **Spec version 2.4.1**, built 2026-07-22T17:07:47.953Z
+ * Wallet Inc. API reference.  **Spec version 2.4.1**, built 2026-07-22T17:41:25.037Z
  *
  * The version of the OpenAPI document: 2.4.1
  * Contact: development@wallet.inc
