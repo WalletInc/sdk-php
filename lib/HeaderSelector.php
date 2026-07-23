29f861900f1d6978658cafc0d743b1e90b056000
johnturnham <support@wallet.inc>
2026-07-23 13:49:16 +0000

    v2.4.1

--- lib/HeaderSelector.php
+++ lib/HeaderSelector.php
@@ -12,7 +12,7 @@
 /**
  * wallet-api
  *
- * Wallet Inc. API reference.  **Spec version 2.4.1**, built 2026-07-22T18:29:22.250Z
+ * Wallet Inc. API reference.  **Spec version 2.4.1**, built 2026-07-23T13:45:36.824Z
  *
  * The version of the OpenAPI document: 2.4.1
  * Contact: development@wallet.inc
