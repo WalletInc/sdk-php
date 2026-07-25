e5edbcd2737ba379aff544d09c182e85745a8542
johnturnham <support@wallet.inc>
2026-07-25 13:33:57 +0000

    v2.4.1

--- lib/Api/RoomRatesApi.php
+++ lib/Api/RoomRatesApi.php
@@ -12,7 +12,7 @@
 /**
  * wallet-api
  *
- * Wallet Inc. API reference.  **Spec version 2.4.1**, built 2026-07-25T12:49:47.896Z
+ * Wallet Inc. API reference.  **Spec version 2.4.1**, built 2026-07-25T13:30:00.050Z
  *
  * The version of the OpenAPI document: 2.4.1
  * Contact: development@wallet.inc
