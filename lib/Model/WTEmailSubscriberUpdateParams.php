666da5a50fa62bd38086d00ae83a6aa6a2957ce6
johnturnham <support@wallet.inc>
2026-07-25 10:51:53 +0000

    v2.4.1

--- lib/Model/WTEmailSubscriberUpdateParams.php
+++ lib/Model/WTEmailSubscriberUpdateParams.php
@@ -13,7 +13,7 @@
 /**
  * wallet-api
  *
- * Wallet Inc. API reference.  **Spec version 2.4.1**, built 2026-07-25T10:26:46.679Z
+ * Wallet Inc. API reference.  **Spec version 2.4.1**, built 2026-07-25T10:48:12.022Z
  *
  * The version of the OpenAPI document: 2.4.1
  * Contact: development@wallet.inc
