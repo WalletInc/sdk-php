4650cd956df4c6719e26cbd221d2a1c87c459345
johnturnham <support@wallet.inc>
2026-08-01 10:30:51 +0000

    v2.20.2

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ and no login, unlocked by one SMS code. This is the official PHP SDK for the Wal
 
 Wallet Inc. API reference.
 
-**Spec version 2.20.2**, built 2026-08-01T08:37:13.444Z
+**Spec version 2.20.2**, built 2026-08-01T10:27:04.585Z
 
 
 ## Installation & Usage
--- lib/Model/FetchIndustry200Response.php
+++ lib/Model/FetchIndustry200Response.php
@@ -13,7 +13,7 @@
 /**
  * wallet-api
  *
- * Wallet Inc. API reference.  **Spec version 2.20.2**, built 2026-08-01T08:37:13.444Z
+ * Wallet Inc. API reference.  **Spec version 2.20.2**, built 2026-08-01T10:27:04.585Z
  *
  * The version of the OpenAPI document: 2.20.2
  * Contact: development@wallet.inc
