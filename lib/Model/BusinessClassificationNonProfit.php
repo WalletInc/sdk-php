545fbb817be97ba4baf55e3f9ec1878c1b487e13
johnturnham <support@wallet.inc>
2026-08-01 11:22:23 +0000

    v2.20.2

--- lib/Model/BusinessClassificationNonProfit.php
+++ lib/Model/BusinessClassificationNonProfit.php
@@ -13,7 +13,7 @@
 /**
  * wallet-api
  *
- * Wallet Inc. API reference.  **Spec version 2.20.2**, built 2026-08-01T10:27:04.585Z
+ * Wallet Inc. API reference.  **Spec version 2.20.2**, built 2026-08-01T11:18:42.360Z
  *
  * The version of the OpenAPI document: 2.20.2
  * Contact: development@wallet.inc
