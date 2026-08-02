044a7e65965cfadba50e9b37fd81e7275ea1aa04
johnturnham <support@wallet.inc>
2026-08-02 17:01:54 +0000

    v2.20.2

--- lib/Model/TrustBundleStatuses.php
+++ lib/Model/TrustBundleStatuses.php
@@ -13,7 +13,7 @@
 /**
  * wallet-api
  *
- * Wallet Inc. API reference.  **Spec version 2.20.2**, built 2026-08-02T16:00:04.267Z
+ * Wallet Inc. API reference.  **Spec version 2.20.2**, built 2026-08-02T16:57:50.829Z
  *
  * The version of the OpenAPI document: 2.20.2
  * Contact: development@wallet.inc
