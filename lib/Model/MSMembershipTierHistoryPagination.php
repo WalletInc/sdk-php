eba478709c0382913b2834c7cc78874dd10adc3f
johnturnham <support@wallet.inc>
2026-08-22 06:12:31 +0000

    v2.38.3

--- lib/Model/MSMembershipTierHistoryPagination.php
+++ lib/Model/MSMembershipTierHistoryPagination.php
@@ -13,7 +13,7 @@
 /**
  * wallet-api
  *
- * Wallet Inc. API reference.  **Spec version 2.38.3**, built 2026-08-22T05:24:14.250Z
+ * Wallet Inc. API reference.  **Spec version 2.38.3**, built 2026-08-22T06:07:08.767Z
  *
  * The version of the OpenAPI document: 2.38.3
  * Contact: development@wallet.inc
